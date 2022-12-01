b158f721698e3a1d742bad6d46ee40c57b1e2012
paunstefan <paunstefan@protonmail.com>
2022-12-01 15:59:58 +0200

    Testing macro

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,50 @@ macro_rules! run_day {
         println!("Part 1: {} ({:?})", p1, duration);
 
         let start = std::time::Instant::now();
-        let p1 = $x::run_part2($input);
+        let p2 = $x::run_part2($input);
         let duration = start.elapsed();
-        println!("Part 2: {} ({:?})", p1, duration);
+        println!("Part 2: {} ({:?})", p2, duration);
+    }};
+}
+
+#[macro_export]
+macro_rules! run_day_test {
+    ( $x:ident, $input:expr, $res:expr ) => {{
+        let results: Vec<i64> = std::fs::read_to_string($res)
+            .unwrap()
+            .lines()
+            .map(|n| n.parse::<i64>().unwrap())
+            .collect();
+
+        if results.is_empty() {
+            println!("No results in file.");
+            return;
+        }
+
+        println!("\nTesting {}:", stringify!($x));
+        let start = std::time::Instant::now();
+        let p1 = $x::run_part1($input);
+        let duration = start.elapsed();
+
+        let good = if results[0] == p1 {
+            "PASSED".to_string()
+        } else {
+            format!("FAILED, expected {}", results[0])
+        };
+        println!("Part 1: {} - {} ({:?})", p1, good, duration);
+
+        if results.len() == 2 {
+            let start = std::time::Instant::now();
+            let p2 = $x::run_part2($input);
+            let duration = start.elapsed();
+
+            let good = if results[1] == p2 {
+                "PASSED".to_string()
+            } else {
+                format!("FAILED, expected {}", results[1])
+            };
+            println!("Part 2: {} - {} ({:?})", p2, good, duration);
+        }
     }};
 }
 
@@ -20,8 +61,19 @@ macro_rules! aoc_days{
         $(mod $day;)*
         pub fn match_day(d: &str) {
             let d = format!("{}{}", "aoc", d);
+
             match &*d {
-                $(stringify!($day) => run_day!($day, &format!("data/{}", stringify!($day))),)*
+                $(stringify!($day) => run_day!($day, &format!("data/inputs/{}", stringify!($day))),)*
+                _ => eprintln!("Day not valid"),
+            }
+        }
+
+        pub fn match_day_test(d: &str){
+            let d = format!("{}{}", "aoc", d);
+
+            match &*d {
+                $(stringify!($day) => run_day_test!($day, &format!("data/tests/{}", stringify!($day)),
+                                        &format!("data/tests/{}_results", stringify!($day))),)*
                 _ => eprintln!("Day not valid"),
             }
         }
